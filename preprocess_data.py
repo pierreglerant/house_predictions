d64335a83c5676efee025b028cdb801f32bb10cb
pierreglerant <154981553+pierreglerant@users.noreply.github.com>
2024-05-28 20:00:32 +0100

    docs: add header of replaceMissingValues

--- preprocess_data.py
+++ preprocess_data.py
@@ -6,4 +6,15 @@ import pandas as pd
 
 # Functions
 def replaceMissingValues(df:pd.DataFrame) -> pd.DataFrame:
+    """Handle missing values in the df
+    
+    First case     (columns composed of numbers) : missing values are replaced by 0
+    Second case (columns composed of categories) : missing values are replaced by None
+
+    Args : 
+        df (pd.DataFrame) : treated DataFrame
+
+    Returns :
+        pd.DataFrame : df with missing values replaced by 0 or None
+    """
     raise NotImplementedError
