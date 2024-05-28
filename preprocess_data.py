29199e6df712b102bc3bf211d34cca03fdcf63f3
pierreglerant <154981553+pierreglerant@users.noreply.github.com>
2024-05-28 19:54:57 +0100

    feat: create a file called preprocess_data.py
    File which contains preprocessing functions

---
+++ preprocess_data.py
@@ -0,0 +1,9 @@
+"""File which contains preprocessing functions
+"""
+
+# Import modules
+import pandas as pd
+
+# Functions
+def replaceMissingValues(df:pd.DataFrame) -> pd.DataFrame:
+    raise NotImplementedError
