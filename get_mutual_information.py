91f568492b9b0f3b8cac98e2b35857adbdfc14b0
pierreglerant <154981553+pierreglerant@users.noreply.github.com>
2024-05-28 19:12:00 +0100

    docs: add header of getMIScores

--- get_mutual_information.py
+++ get_mutual_information.py
@@ -7,6 +7,17 @@ import pandas as pd
 
 # Functions
 def getMIScores(X:pd.DataFrame,y:pd.Series,discreateFeatures:pd.Series) -> pd.Series:
+    """ Get the mutual information scores between y and every columns of X
+
+    Args:
+        -   X              (pd.DataFrame) : feature matrix
+        -   y                 (pd.Series) : target variable
+        -   discreateFeatures (pd.Series) : series with X's columns as index composed of booleans
+                                           (X.columnName.dtype == int)
+    
+    Returns:
+        pd.Series : mutual information scores between y and every columns of X
+    """
     raise NotImplementedError
 
 def plotMIBar(miScores:pd.Series,miScoresIndex:list):
