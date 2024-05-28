590be8083f6b24fb59108662b13f18f25210dc73
pierreglerant <154981553+pierreglerant@users.noreply.github.com>
2024-05-28 19:24:51 +0100

    style: add a white-space in the header of getMIScores

--- get_mutual_information.py
+++ get_mutual_information.py
@@ -9,7 +9,7 @@ import pandas as pd
 def getMIScores(X:pd.DataFrame,y:pd.Series,discreateFeatures:pd.Series) -> pd.Series:
     """ Get the mutual information scores between y and every columns of X
 
-    Args:
+    Args :
         -   X              (pd.DataFrame) : feature matrix
         -   y                 (pd.Series) : target variable
         -   discreateFeatures (pd.Series) : series with X's columns as index composed of booleans
